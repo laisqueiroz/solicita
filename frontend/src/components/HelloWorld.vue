e543c1e7cecefab55f57032dae0c6794be79ef58
Lais Queiroz <lais.frequeiroz@gmail.com>
2025-02-04 19:21:23 -0300

    configurado o frontend

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+</script>
+
+<template>
+  <button>
+    botão
+  </button>
+
+  <button class="btn-primary">
+    primário
+  </button>
+
+  <button class="btn-secondary">
+    secundário
+  </button>
+
+  <button class="btn-add">
+    add
+  </button>
+
+  <button class="btn-remove">
+    remove
+  </button>
+
+  <button class="btn-no-filling">
+    primário 2
+  </button>
+
+  <button class="btn-no-filling-sec">
+    secundário 2
+  </button>
+
+  <button class="btn-no-filling-add">
+    add
+  </button>
+
+  <button class="btn-no-filling-remove">
+    remover
+  </button>
+</template>
+
+<style scoped>
+
+</style>
