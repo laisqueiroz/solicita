408495980fcfac5a93b1935be8c94c4725ce99c7
iohanalopes <111798386+iohanalopes@users.noreply.github.com>
2025-02-26 17:52:09 -0300

    espaçamentos HomePage.vue

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -13,48 +13,47 @@
 </template>
 
 <script setup>
-import HeaderComponent from '../components/HeaderHome.vue';
-import FooterComponent from '../components/FooterIllustration.vue';
-
+    import HeaderComponent from '../components/HeaderHome.vue';
+    import FooterComponent from '../components/FooterIllustration.vue';
 </script>
 
 <style scoped>
-#app {
-max-width: min-content;
-margin: 0px;
-padding: 0px;
-}
-main {
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding: 0 40px;
-height: calc(100vh - 80px);
-}
-.container {
-display: grid;
-grid-template-columns: repeat(5, 20%);
-grid-template-rows: 25% 25% 25% 25%;
-}
-.content {
-grid-column-start: 4;
-grid-column-end: 6;
-grid-row-start: 2;
-grid-row-end: 4;
-}
-.content h1 {
-text-align: left;
-font-size: 30px;
-color: #f7981d;
-line-height: 1.2;
-font-weight: bold;
-}
-.content p {
-color: #fff;
-text-align: left;
-font-size: 14px;
-margin-top: 20px;
-line-height: 1.6;
-padding-right: 100px;
-}
+    #app {
+    max-width: min-content;
+    margin: 0px;
+    padding: 0px;
+    }
+    main {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 40px;
+    height: calc(100vh - 80px);
+    }
+    .container {
+    display: grid;
+    grid-template-columns: repeat(5, 20%);
+    grid-template-rows: 25% 25% 25% 25%;
+    }
+    .content {
+    grid-column-start: 4;
+    grid-column-end: 6;
+    grid-row-start: 2;
+    grid-row-end: 4;
+    }
+    .content h1 {
+    text-align: left;
+    font-size: 30px;
+    color: #f7981d;
+    line-height: 1.2;
+    font-weight: bold;
+    }
+    .content p {
+    color: #fff;
+    text-align: left;
+    font-size: 14px;
+    margin-top: 20px;
+    line-height: 1.6;
+    padding-right: 100px;
+    }
 </style>
